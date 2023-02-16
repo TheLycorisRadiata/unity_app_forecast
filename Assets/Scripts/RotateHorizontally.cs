23d3ac7d90985a9175780abce7e66699ac2c7eeb
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-02-16 22:00:52 +0100

    Actual globe rotation fix

--- Assets/Scripts/RotateHorizontally.cs
+++ Assets/Scripts/RotateHorizontally.cs
@@ -1,18 +1,15 @@
 using UnityEngine;
 
-public class RotateEarth : MonoBehaviour
+public class RotateHorizontally : MonoBehaviour
 {
     [SerializeField] private UserInput input;
 
     private float rotateSpeed = 15f;
-    private float yaw; // rotation around Y
     private float pitch; // rotation around X
 
     void FixedUpdate()
     {
-        yaw += input.movementVector.y * rotateSpeed * Time.deltaTime;
         pitch += -input.movementVector.x * rotateSpeed * Time.deltaTime;
-
-        transform.localRotation = Quaternion.Euler(yaw, pitch, 0f);
+        transform.localRotation = Quaternion.Euler(0f, pitch, 0f);
     }
 }
