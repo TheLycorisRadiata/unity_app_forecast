21b3fbc66546cd8c337e15663a29e59ecfd10059
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-02-17 16:23:26 +0100

    Make the raycast point public so it's accessible from another script

--- Assets/Scripts/WebglRaycast.cs
+++ Assets/Scripts/WebglRaycast.cs
@@ -6,21 +6,16 @@ using static UnityEditor.PlayerSettings;
 
 public class WebglRaycast : MonoBehaviour
 {
-
-    [SerializeField] Vector3 RaycastPoint = Vector3.zero;
+    public Vector3 RaycastPoint = Vector3.zero;
     Camera cam;
-   
 
-    // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-
         //draw ray
         Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
         Debug.DrawRay(ray.origin, ray.direction * 90, Color.blue);
