7806eab5ce74078553591c7fb73a1ff26ea26d49
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-02-19 20:15:42 +0100

    Decouple the cartesian and polar coordinates into different scripts + More detailed comments

--- Assets/Scripts/CartesianCoordinates.cs
+++ Assets/Scripts/CartesianCoordinates.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CartesianCoordinates : MonoBehaviour
@@ -7,6 +5,7 @@ public class CartesianCoordinates : MonoBehaviour
     [SerializeField] private WebglRaycast raycastScript;
     [SerializeField] private RotateHorizontally horizontalRotationScript;
     [SerializeField] private RotateVertically verticalRotationScript;
+    public float xCoordinate, yCoordinate;
 
     void Update()
     {
@@ -14,13 +13,32 @@ public class CartesianCoordinates : MonoBehaviour
             TODO
             - [X] Get the raycast's vector3, the horizontal rotation (pitch) and the vertical rotation (yaw).
             - [X] Instead of using the raycast vector3 as is, add an offset with the earth's rotations.
-            - [X] Reset the rotations to 0 if they are outside of -360/360.
+            - [X] Rotation scripts: Reset the rotations to 0 if they are outside of -360/360.
             - [X] Convert the cartesian Y coordinate to the latitude equivalent.
             - [X] Convert the cartesian X coordinate to the longitude equivalent.
-            - [ ] Add the earth curvature to the cartesian coordinates calculus.
-            - [ ] Don't forget to take the Earth default rotation into account (e.g. -250 on the y axis), 
-                  so that any chosen default rotation doesn't break the coordinates.
-                  The 0x0 polar coordinates need to be at null island (around Africa).
+
+            NOTE: Only the X and Y values of the raycast have been used so far. This means that the coordinates are only correct when the cursor is right on the middle of the sphere.
+            This can be fixed by adding the Z raycast value as an offset, but keep in mind that the Z raycast value changes depending on the zoom (camera position on the Z axis).
+            - [ ] Add the Earth curvature to the cartesian coordinates computation.
+
+            NOTE: The initial Earth rotation ("Model" gameobject) has its importance and is currently: 0, -250, 0.
+            No matter the initial rotation, null island needs to be the 0x0 coordinates, it's around Africa.
+            Once the horizontal and vertical rotations have been added to the cartesian coordinates as an offset (which is done), another offset needs to be added so that the coordinates are relative to null island.
+            - [ ] Take the Earth default rotation into account, so that any chosen default rotation doesn't break the coordinates.
+        */
+
+        /*
+            CURVATURE PROBLEM
+            - zRaycast seems to be -47.5 at 0x0, and this no matter the zoom.
+            - But, when we aim for the horizon of the sphere, zRaycast changes depending on the zoom.
+
+            - Another observation is that around ~20 is added to the latitude when its sampled from the horizon and not 0x0.
+            - I haven't checked for the longitude.
+            - This is for the default zoom, the observed offset may be different from ~20 with another zoom.
+
+            - Otherwise, I have the beginning of a calculus:
+                * zRaycast + 47.5 --> Result is a number between 0 and ~25 (at default zoom)
+                * latitude - result --> Correct latitude
         */
 
         float xRotation = horizontalRotationScript.pitch;
@@ -30,47 +48,13 @@ public class CartesianCoordinates : MonoBehaviour
         float yRaycast = raycastScript.RaycastPoint.y;
         float zRaycast = raycastScript.RaycastPoint.z;
 
-        float xRaycastWithOffset = xRaycast + xRotation;
-        float yRaycastWithOffset = yRaycast + yRotation;
-
-        // Latitude goes first
-        float latitude = yRaycastWithOffset;
-        float longitude = xRaycastWithOffset;
-
-        // If you go north until you do a full 360
-        if (yRaycastWithOffset > 90f && yRaycastWithOffset <= 270f)
-            latitude = (yRaycastWithOffset - 180f) * -1f;
-        else if (yRaycastWithOffset > 270f && yRaycastWithOffset <= 360f)
-            latitude = yRaycastWithOffset - 360f;
-
-        // If you go south until you do a full 360
-        if (yRaycastWithOffset < -90f && yRaycastWithOffset >= -270f)
-            latitude = yRaycastWithOffset * -1f - 180f;
-        else if (yRaycastWithOffset < -270f && yRaycastWithOffset >= -360f)
-            latitude = yRaycastWithOffset + 360f;
-
-        // If you go east
-        if (xRaycastWithOffset > 180f && xRaycastWithOffset <= 360f)
-            longitude = xRaycastWithOffset - 360f;
-        // If you go west
-        else if (xRaycastWithOffset <= -180f && xRaycastWithOffset >= -360f)
-            longitude = xRaycastWithOffset + 360f;
+        xCoordinate = xRaycast + xRotation;
+        yCoordinate = yRaycast + yRotation;
 
         //Debug.Log("[Rotation] X: " + xRotation + " / Y: " + yRotation);
-        Debug.Log("[Raycast] X (no offset): " + xRaycast + " / X (with offset): " + xRaycastWithOffset);
-        Debug.Log("[Raycast] Y (no offset): " + yRaycast + " / Y (with offset): " + yRaycastWithOffset);
-        Debug.Log("[Raycast] Z: " + zRaycast);
-        Debug.Log("[Polar] Latitude: " + latitude + " / Longitude: " + longitude);
-        Debug.Log("-------------");
-
-        /*
-            CURVATURE PROBLEM
-            - zRaycast is -47.5 at 0x0, and this no matter the zoom.
-            - But, when we aim for the horizon of the sphere, zRaycast changes depending on the zoom.
-
-            - Otherwise, I have the beginning of a calculus:
-                * zRaycast + 47.5 --> Result is a number between 0 and ~25 (at default zoom)
-                * latitude - result --> Correct latitude (around +20 is added when latitude is sampled from the horizon and not 0x0)
-        */
+        //Debug.Log("[Raycast] X (no offset): " + xRaycast + " / X (with offset): " + xCoordinate);
+        //Debug.Log("[Raycast] Y (no offset): " + yRaycast + " / Y (with offset): " + yCoordinate);
+        //Debug.Log("[Raycast] Z: " + zRaycast);
+        //Debug.Log("-------------");
     }
 }
---
+++ Assets/Scripts/PolarCoordinates.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolarCoordinates : MonoBehaviour
+{
+    [SerializeField] private CartesianCoordinates cartesian;
+    public float latitude, longitude;
+
+    void Update()
+    {
+        latitude = cartesian.yCoordinate;
+        longitude = cartesian.xCoordinate;
+
+        // If you go north until you do a full 360
+        if (cartesian.yCoordinate > 90f && cartesian.yCoordinate <= 270f)
+            latitude = (cartesian.yCoordinate - 180f) * -1f;
+        else if (cartesian.yCoordinate > 270f && cartesian.yCoordinate <= 360f)
+            latitude = cartesian.yCoordinate - 360f;
+
+        // If you go south until you do a full 360
+        if (cartesian.yCoordinate < -90f && cartesian.yCoordinate >= -270f)
+            latitude = cartesian.yCoordinate * -1f - 180f;
+        else if (cartesian.yCoordinate < -270f && cartesian.yCoordinate >= -360f)
+            latitude = cartesian.yCoordinate + 360f;
+
+        // If you go east
+        if (cartesian.xCoordinate > 180f && cartesian.xCoordinate <= 360f)
+            longitude = cartesian.xCoordinate - 360f;
+        // If you go west
+        else if (cartesian.xCoordinate <= -180f && cartesian.xCoordinate >= -360f)
+            longitude = cartesian.xCoordinate + 360f;
+
+        //Debug.Log("Latitude: " + latitude + " / Longitude: " + longitude);
+    }
+}
