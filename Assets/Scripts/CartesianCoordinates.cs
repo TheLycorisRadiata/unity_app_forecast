2aea7dea98a4d5a9f2e3d9843238f2551c231ae3
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-02-20 16:46:36 +0100

    Compute the offset of the initial Earth model rotation relative to Null Island

--- Assets/Scripts/CartesianCoordinates.cs
+++ Assets/Scripts/CartesianCoordinates.cs
@@ -14,12 +14,33 @@ public class CartesianCoordinates : MonoBehaviour
         This value is used to normalize the current Z raycast value, so it's 0 on the center and a positive value everywhere else.
     */
 
-    private Vector3 modelRotationForCenteredNullIsland = new Vector3(0f, -270f, -1);
+    private Vector3 modelRotationOffset;
     /*
         On the actual Earth, Null Island is on the 0x0 coordinates.
         The Earth model needs to be rotated a certain way for its Null Island to be centered.
         This vector3 is used as an offset for the cartesian coordinates computation.
     */
+
+    void Start()
+    {
+        /*
+            In the inspector, Null Island is centered at (0, -270, -1) but the euler angles are (0, 90, 359).
+            The euler angles need to be used in script, as it's not possible to access the inspector rotation values.
+            Also note that 360 is the same as 0, and vice versa.
+        */
+
+        Vector3 modelRotationForCenteredNullIsland = new Vector3(360f, 90f, 359f);
+        Vector3 modelRotation = transform.eulerAngles;
+
+        if (modelRotation.x == 0f)
+            modelRotation.x = 360f;
+        if (modelRotation.y == 0f)
+            modelRotation.y = 360f;
+        if (modelRotation.z == 0f)
+            modelRotation.z = 360f;
+
+        modelRotationOffset = modelRotationForCenteredNullIsland - modelRotation;
+    }
 
     void Update()
     {
@@ -31,15 +52,15 @@ public class CartesianCoordinates : MonoBehaviour
             - [X] Convert the cartesian Y coordinate to the latitude equivalent.
             - [X] Convert the cartesian X coordinate to the longitude equivalent.
 
-            NOTE: Only xRaycast and yRaycast have been used so far. This means that the coordinates are only correct when the cursor is right on the middle of the sphere.
-            This can be fixed by adding zRaycast as an offset, but keep in mind that zRaycast changes depending on the zoom (camera position on the Z axis).
-            - [ ] Add the Earth curvature to the cartesian coordinates computation.
-
             NOTE: The initial Earth rotation ("Model" gameobject) has its importance and is currently (0, -250, 0).
             No matter the initial rotation, null island needs to be the 0x0 coordinates, it's around Africa.
             Once the horizontal and vertical rotations have been added to the cartesian coordinates as an offset (which is done),
             another offset needs to be added so that the coordinates are relative to null island.
             - [ ] Take the Earth default rotation into account, so that any chosen default rotation doesn't break the coordinates.
+
+            NOTE: Only xRaycast and yRaycast have been used so far. This means that the coordinates are only correct when the cursor is right on the middle of the sphere.
+            This can be fixed by adding zRaycast as an offset, but keep in mind that zRaycast changes depending on the zoom (camera position on the Z axis).
+            - [ ] Add the Earth curvature to the cartesian coordinates computation.
         */
 
         float xRotation = horizontalRotationScript.pitch;
@@ -64,6 +85,7 @@ public class CartesianCoordinates : MonoBehaviour
             - CALCULUS: latitude - zRaycast --> Correct latitude?
         */
 
+        //Debug.Log("[Model rotation offset] " + modelRotationOffset);
         //Debug.Log("[Rotation] X: " + xRotation + " / Y: " + yRotation);
         //Debug.Log("[Raycast] X (no offset): " + xRaycast + " / X (with rotation offset): " + xCoordinate);
         //Debug.Log("[Raycast] Y (no offset): " + yRaycast + " / Y (with rotation offset): " + yCoordinate);
