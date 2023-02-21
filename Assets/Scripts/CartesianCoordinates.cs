527f60e30624245def2988137d5ebf168c5e16f8
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-02-21 12:17:47 +0100

    Fixed coordinates calculation

--- Assets/Scripts/CartesianCoordinates.cs
+++ Assets/Scripts/CartesianCoordinates.cs
@@ -1,24 +1,35 @@
-using System;
 using UnityEngine;
 
 public class CartesianCoordinates : MonoBehaviour
 {
-    public float xCoordinate, yCoordinate;
+    public Vector3 coordinates;
+    private Vector3 modelRotationOffset;
+
     [SerializeField] private WebglRaycast raycastScript;
     [SerializeField] private RotateHorizontally horizontalRotationScript;
     [SerializeField] private RotateVertically verticalRotationScript;
-    private Vector3 modelRotationOffset;
-    /*
-        The Z raycast value when sampled from the center of the sphere seems to be -47.434.
-        I couldn't find the way to save the value automatically, and resorted to run manual tests to find it.
-        Note that the absolute value is used, hence the multiplication by -1.
-    */
-    private float zRaycastCenter = -47.434f * -1f;
-
+ 
     void Start()
     {
+        modelRotationOffset = RotationOffsetRelativeToNullIsland();
+    }
+
+    void Update()
+    {
+        float xRotationOffset = horizontalRotationScript.pitch - modelRotationOffset.y;
+        float yRotationOffset = modelRotationOffset.z - verticalRotationScript.yaw;
+
+        coordinates = raycastScript.RaycastPoint;
+        coordinates.x += xRotationOffset;
+        coordinates.y += yRotationOffset;
+
+        // CHECK: Real latitude and longitude --> Nice (43.7, 7.2) and London (51.5, 0)
+    }
+
+    private Vector3 RotationOffsetRelativeToNullIsland()
+    {
         /*
-            Null Island is at the 0x0 polar coordinates.
+            Null Island is at the (0,0) polar coordinates.
             In the inspector, Null Island is centered at (0, -272.4, -2) but the euler angles are (0, 87.6, 358).
             The euler angles need to be used in script, as it's not possible to access the inspector rotation values.
             Also note that 360 is the same as 0, and vice versa.
@@ -37,41 +48,6 @@ public class CartesianCoordinates : MonoBehaviour
         if (modelRotation.z == 0f)
             modelRotation.z = 360f;
 
-        modelRotationOffset = modelRotationForCenteredNullIsland - modelRotation;
-    }
-
-    void Update()
-    {
-        /*
-            TODO
-
-            Only xRaycast and yRaycast have been used so far. This means that the coordinates are only correct when the cursor is right on the middle of the sphere.
-            This can be fixed by adding zRaycast as an offset.
-            - [ ] Add the Earth curvature to the cartesian coordinates computation.
-        */
-
-        float xRaycast = raycastScript.RaycastPoint.x;
-        float yRaycast = raycastScript.RaycastPoint.y;
-        float zRaycast = Math.Abs(raycastScript.RaycastPoint.z);
-
-        float xRotationOffset = horizontalRotationScript.pitch;
-        float yRotationOffset = -verticalRotationScript.yaw;
-
-        xCoordinate = xRaycast + xRotationOffset - modelRotationOffset.y;
-        yCoordinate = yRaycast + yRotationOffset + modelRotationOffset.z;
-
-        // The inclination and the azimuth are degrees
-        float radius = zRaycastCenter;
-        float distanceToSphere = radius - zRaycast;
-        float inclination = (float)Math.Acos(distanceToSphere / radius);
-        float azimuth = (float)Math.Atan2(yCoordinate, xCoordinate);
-
-        xCoordinate += azimuth;
-        yCoordinate += inclination;
-
-        /*
-            CURVATURE PROBLEM
-            - Real latitude and longitude: Nice (43.7, 7.2) and London (51.5, 0).
-        */
+        return modelRotationForCenteredNullIsland - modelRotation;
     }
 }
--- Assets/Scripts/PolarCoordinates.cs
+++ Assets/Scripts/PolarCoordinates.cs
@@ -5,30 +5,12 @@ using UnityEngine;
 public class PolarCoordinates : MonoBehaviour
 {
     public float latitude, longitude;
-    [SerializeField] private CartesianCoordinates cartesian;
+    [SerializeField] private CartesianCoordinates cartesianScript;
 
     void Update()
     {
-        latitude = cartesian.yCoordinate;
-        longitude = cartesian.xCoordinate;
-
-        // If you go north until you do a full 360
-        if (cartesian.yCoordinate > 90f && cartesian.yCoordinate <= 270f)
-            latitude = (cartesian.yCoordinate - 180f) * -1f;
-        else if (cartesian.yCoordinate > 270f && cartesian.yCoordinate <= 360f)
-            latitude = cartesian.yCoordinate - 360f;
-
-        // If you go south until you do a full 360
-        if (cartesian.yCoordinate < -90f && cartesian.yCoordinate >= -270f)
-            latitude = cartesian.yCoordinate * -1f - 180f;
-        else if (cartesian.yCoordinate < -270f && cartesian.yCoordinate >= -360f)
-            latitude = cartesian.yCoordinate + 360f;
-
-        // If you go east
-        if (cartesian.xCoordinate > 180f && cartesian.xCoordinate <= 360f)
-            longitude = cartesian.xCoordinate - 360f;
-        // If you go west
-        else if (cartesian.xCoordinate <= -180f && cartesian.xCoordinate >= -360f)
-            longitude = cartesian.xCoordinate + 360f;
+        Vector3 dir = cartesianScript.coordinates.normalized;
+        latitude = Mathf.Asin(dir.y) * Mathf.Rad2Deg;
+        longitude = Mathf.Atan2(dir.x, - dir.z) * Mathf.Rad2Deg;
     }
 }
