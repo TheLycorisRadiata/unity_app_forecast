269456d518ff78e756aade1edc7804fd95f89d29
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2023-02-21 16:45:05 +0100

    Fix null island offset

--- Assets/Scripts/CartesianCoordinates.cs
+++ Assets/Scripts/CartesianCoordinates.cs
@@ -22,32 +22,40 @@ public class CartesianCoordinates : MonoBehaviour
         coordinates = raycastScript.RaycastPoint;
         coordinates.x += xRotationOffset;
         coordinates.y += yRotationOffset;
-
-        // CHECK: Real latitude and longitude --> Nice (43.7, 7.2) and London (51.5, 0)
     }
 
     private Vector3 RotationOffsetRelativeToNullIsland()
     {
         /*
-            Null Island is at the (0,0) polar coordinates.
-            In the inspector, Null Island is centered at (0, -272.4, -2) but the euler angles are (0, 87.6, 358).
-            The euler angles need to be used in script, as it's not possible to access the inspector rotation values.
-            Also note that 360 is the same as 0, and vice versa.
+            Null Island is rotated at (0, -270.5, 1.5) in the inspector, 
+            and in euler angles it's (0, 89.5, 1.5).
         */
 
-        Vector3 modelRotationForCenteredNullIsland = new Vector3(360f, 87.6f, 358f);
+        Vector3 modelRotationForCenteredNullIsland = new Vector3(360f, -270.5f, 1.5f);
         Vector3 modelRotation = transform.eulerAngles;
+        Vector3 offset = Vector3.zero;
 
         // unused in our context
         if (modelRotation.x == 0f)
             modelRotation.x = 360f;
+
         // horizontal rotation
         if (modelRotation.y == 0f)
             modelRotation.y = 360f;
+
         // vertical rotation
         if (modelRotation.z == 0f)
             modelRotation.z = 360f;
 
-        return modelRotationForCenteredNullIsland - modelRotation;
+        offset = modelRotationForCenteredNullIsland - modelRotation;
+
+        if (offset.x < 0)
+            offset.x += 360f;
+        if (offset.y < 0)
+            offset.y += 360f;
+        if (offset.z < 0)
+            offset.z += 360f;
+
+        return offset;
     }
 }
--- Assets/Scripts/PolarCoordinates.cs
+++ Assets/Scripts/PolarCoordinates.cs
@@ -12,5 +12,7 @@ public class PolarCoordinates : MonoBehaviour
         Vector3 dir = cartesianScript.coordinates.normalized;
         latitude = Mathf.Asin(dir.y) * Mathf.Rad2Deg;
         longitude = Mathf.Atan2(dir.x, - dir.z) * Mathf.Rad2Deg;
+
+        // CHECK: Real latitude and longitude --> Nice (43.7, 7.2) and London (51.5, 0)
     }
 }
